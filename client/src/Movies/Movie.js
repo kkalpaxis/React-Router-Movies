a3f3347c4bcc9e343aeb6f92a18f4d97e22e822f
Karina Kalpaxis <kalpaxis1@gmail.com>
2019-07-22 20:00:09 -0400

    Post Standup Progress: nearly at MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
- 
+  const [movie, setMovie] = useState();
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -13,12 +12,18 @@ const Movie = (props) => {
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
+          console.log(response.data)
         })
         .catch(error => {
           console.error(error);
         });
 
   },[]);
+  
+
+
+
+
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from "react-router-dom";
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -30,6 +31,7 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
+    // <Link to={ } />
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
